4588f0cdb5b0750dff0def647870903972f498ae
unknown <asadov2610a@gmail.com>
2023-10-10 19:06:31 +0300

    6th review

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -6,8 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'abcdefghijklmonp')
 
-# DEBUG = os.getenv('DEBUG', 'False') == 'True'
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(', ')
 
